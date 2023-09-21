67f6f801a0b82d1299192ed17a96e34a38fe17b4
sakai <amu2@sakai.pro>
2023-09-22 00:13:36 +0900

    OLED出力関数

--- receiver/disp.py
+++ receiver/disp.py
@@ -9,29 +9,24 @@ i2c = busio.I2C(board.GP17, board.GP16)  # (SCL端子, SDA端子)
 #  液晶画面設定
 display = adafruit_ssd1306.SSD1306_I2C(128, 64, i2c, addr=0x3C) # 画面サイズ幅,  高さ, 通信仕様, デバイスアドレス
 
-#  画面表示
-display.fill(0) # 画面表示初期化
+# 引数に表示したいアドレス(0~99)の配列を指定するとOLEDに出力
+# 表示可能な配列は最大8個
+def showAdress(addressList):
+    display.fill(0) # 画面表示初期化
+    rect_x = [0, 32, 64, 96, 0, 32, 64, 96]
+    rect_y = [0, 0, 0, 0, 29, 29, 29, 29]
+    text_x = [3, 35, 67, 99, 3, 35, 67, 99]
+    text_y = [7, 7, 7, 7, 36, 36, 36, 36]
+    
+    for i in range(len(addressList)):
+        # 文字表示（"表示内容", x座標, y座標, 色, フォント, サイズ[倍率]）
+        display.rect(rect_x[i], rect_y[i], 28, 28, 1)
+        number_str = str(addressList[i])
+        zero_padded = "0" * (2 - len(number_str)) + number_str
+        display.text(zero_padded, text_x[i], text_y[i], 1, font_name="font5x8.bin", size=2)
 
-# 文字表示（"表示内容", x座標, y座標, 色, フォント, サイズ[倍率]）
+   
+    #display.text('receiving.......', 0, 57, 1, font_name="font5x8.bin", size=1)
+    display.show()  # 画面表示実行
 
-
-display.rect(0, 0, 28, 28, 1)
-display.rect(32, 0, 28, 28, 1)
-display.rect(64, 0, 28, 28, 1)
-display.rect(96, 0, 28, 28, 1)
-display.rect(0, 29, 28, 28, 1)
-display.rect(32, 29, 28, 28, 1)
-display.rect(64, 29, 28, 28, 1)
-display.rect(96, 29, 28, 28, 1)
-
-display.text("00", 3, 7, 1, font_name="font5x8.bin", size=2)
-display.text("01", 35, 7, 1, font_name="font5x8.bin", size=2)
-display.text("02", 67, 7, 1, font_name="font5x8.bin", size=2)
-display.text("03", 99, 7, 1, font_name="font5x8.bin", size=2)
-display.text("04", 3, 36, 1, font_name="font5x8.bin", size=2)
-display.text("05", 35, 36, 1, font_name="font5x8.bin", size=2)
-display.text("06", 67, 36, 1, font_name="font5x8.bin", size=2)
-display.text("07", 99, 36, 1, font_name="font5x8.bin", size=2)
-
-display.text('receiving.......', 0, 57, 1, font_name="font5x8.bin", size=1)
-display.show()  # 画面表示実行
+#showAdress([0,1,2,99,12,13,15])
