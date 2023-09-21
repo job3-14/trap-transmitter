7b405fd57b99b28878306ec320edc186316ce4e8
sakai <amu2@sakai.pro>
2023-09-22 02:01:32 +0900

    送信内容変更
    製品記号+バージョン+個体識別番号(v1は10桁)+登録番号

--- transmitter/config.py
+++ transmitter/config.py
@@ -2,9 +2,10 @@
 version = 'v1' #プログラムバージョン
 serial = 'FnEC3r9ptE' #シリアルID
 SENDING_SENSE = 60
+number = 0
 ######################################
 
 
-# HEX_DATA = 製品記号+バージョン+個体識別番号(v1は10桁)
-HEX_DATA = f'j314trp+{version}+{serial}'.encode('utf-8').hex()
+# HEX_DATA = 製品記号+バージョン+個体識別番号(v1は10桁)+登録番号
+HEX_DATA = f'j314trp+{version}+{serial}-{number}'.encode('utf-8').hex()
 SENDING_SENSE_MS = SENDING_SENSE * 1000
