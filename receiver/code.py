6c4bcfed60a290338b294b527a6851f72a9b410c
sakai <amu2@sakai.pro>
2023-09-22 02:34:22 +0900

    画面切り替え追加

--- receiver/code.py
+++ receiver/code.py
@@ -6,7 +6,7 @@ import disp
 import config
 
 uart = busio.UART(tx=board.GP0, rx=board.GP1, baudrate=115200)
-
+disp.boot()
 
 uart.write(b'mod factory_reset\r\n')
 print(uart.read())
@@ -21,6 +21,7 @@ print(uart.read())
 uart.write(b'p2p set_cr 4/5\r\n')
 print(uart.read())
 
+disp.receiving()
 receive_address_list =[]
 while True:
     uart.write(b'p2p rx 0\r\n') #受信
@@ -31,18 +32,17 @@ while True:
         time.sleep(1)
     if rxData is not None:
         if 'radio_rx' in rxData:
+            # 文字列に変換
             rxDataList = rxData.split()
             hexData = rxDataList[2].decode('utf-8')
             byte_data = binascii.unhexlify(hexData)
             hexData = byte_data.decode('utf-8')
-            if config.RECEIVE_ADDRESS in hexData:
+            # アドレスを画面に表示
+            if config.RECEIVE_ADDRESS in hexData: #シリアル番号等が一致しているか確認
                 index = hexData.find('-')
-                number = int(hexData[index+1:])
-                if (index != -1) and (number < 100) :
-                    receive_address_list.append(number)
-                    if len(receive_address_list) > 9:
+                number = int(hexData[index+1:]) #認識番号
+                if (index != -1) and (number < 100) and (number not in receive_address_list):
+                    receive_address_list.append(number) #受信リストに番号を追加
+                    if len(receive_address_list) > 8: # 8個を超えた場合リスト先頭を削除
                         receive_address_list.pop(0)
-                    disp.showAdress(receive_address_list)
-            print(receive_address_list)
-                    
-                    
+                    disp.showAdress(receive_address_list) # 画面表示関数を実行
